be0f41abdd75122755187d033e690e86de4f9b09
roman <sadovrs@csr43.ru>
2023-02-14 10:44:03 +0300

    исправлен баг с записью только "да" в ячейки T-X новая мини-программа для сборки

--- SvodOperators.py
+++ SvodOperators.py
@@ -7,7 +7,7 @@ from operator import itemgetter
 import numpy as np
 import os
 
-# просто крутилка в консоли, а не о чем вы подумали
+# просто крутилка в консоли
 spinner = Spinner('Обработка данных:')
 
 
@@ -57,33 +57,39 @@ def data_sum_columns(columns_for_sum):
     sum = np.sum(columns_for_sum, axis=0)
     return sum
 
-    # region этот код выбирал из файла только данные по мтс, мегафону, билайну и теле2
-    # def data_filter_operators(data):
-    #     data = []
 
-    #     for i in data:
-    #         if ('МегаФон' in i[7]) or ('МТС' in i[7]) or ('мтс' in i[7]) or ('Т2' in i[7]) or ('Вымпел' in i[7]):
-    #             data.append(i)
-
-    #     return data
-    # endregion
-
-
-def write_new_row(i, check_new_fias, work_sheet, max_speed, columns_for_sum, is_ts, is_uslugi_svyazi, count_row):
+def write_last_row(i, check_new_fias, work_sheet, max_speed, columns_for_sum, is_ts, is_uslugi_svyazi, is_gsm, is_umts, is_lte, count_row):
+    # запись последней строки в файле
+    # сделано пока так, потому что потому
     ts_str = 'нет'
     uslugi_svyazi_str = 'нет'
+    gsm_str = 'нет'
+    umts_str = 'нет'
+    lte_str = 'нет'
 
     if is_ts:
         ts_str = 'да'
     if is_uslugi_svyazi:
         uslugi_svyazi_str = 'да'
+    if is_gsm:
+        gsm_str = 'да'
+    if is_umts:
+        umts_str = 'да'
+    if is_lte:
+        lte_str = 'да'
 
     is_ts = False
     is_uslugi_svyazi = False
+    is_gsm = False
+    is_umts = False
+    is_lte = False
 
-    work_sheet['T' + str(count_row)] = ts_str  # ТС
+    work_sheet['W' + str(count_row)] = ts_str  # ТС
     # Услуги связи
-    work_sheet['U' + str(count_row)] = uslugi_svyazi_str
+    work_sheet['X' + str(count_row)] = uslugi_svyazi_str
+    work_sheet['T' + str(count_row)] = gsm_str  # GSM
+    work_sheet['U' + str(count_row)] = umts_str  # UMTS
+    work_sheet['V' + str(count_row)] = lte_str  # LTE
 
     for row in work_sheet.iter_rows(min_row=count_row, max_row=count_row, min_col=8, max_col=19):
         for cell in row:
@@ -98,10 +104,10 @@ def write_new_row(i, check_new_fias, work_sheet, max_speed, columns_for_sum, is_
         work_sheet['L' + str(count_row)].value)+'; МегаФон - LTE - '+str(work_sheet['M' + str(count_row)].value)+'; '
     svod_beeline = 'Билайн - GSM - '+str(work_sheet['N' + str(count_row)].value)+'; Билайн - UMTS - '+str(
         work_sheet['O' + str(count_row)].value)+'; Билайн - LTE - '+str(work_sheet['P' + str(count_row)].value)+'; '
-    svod_tele2 = 'Т2 - GSM - '+str(work_sheet['Q' + str(count_row)].value)+'; Т2 - UMTS - '+str(
-        work_sheet['R' + str(count_row)].value)+'; Т2 - LTE - '+str(work_sheet['S' + str(count_row)].value)+';'
+    svod_tele2 = 'Теле2 - GSM - '+str(work_sheet['Q' + str(count_row)].value)+'; Теле2 - UMTS - '+str(
+        work_sheet['R' + str(count_row)].value)+'; Теле2 - LTE - '+str(work_sheet['S' + str(count_row)].value)+';'
 
-    work_sheet['AA' + str(count_row)] = svod_mts + \
+    work_sheet['AD' + str(count_row)] = svod_mts + \
         svod_megafon + svod_beeline + svod_tele2
 
     # замена ячеек с циферами на 'да'
@@ -115,23 +121,18 @@ def write_new_row(i, check_new_fias, work_sheet, max_speed, columns_for_sum, is_
         work_sheet['Q' + str(count_row)] = 'да'
 
     # запись максимальной скорости
-    work_sheet['V' + str(count_row)] = data_filter_max_speed(max_speed)
+    work_sheet['Y' + str(count_row)] = data_filter_max_speed(max_speed)
     max_speed.clear()
 
     sum = data_sum_columns(columns_for_sum)
-    work_sheet['W' + str(count_row)] = sum[0]  # кол-во каналов
-    work_sheet['X' + str(count_row)] = sum[1]  # кол-во таксофонов
-    work_sheet['Y' + str(count_row)] = sum[2]  # кол-во точек доступа
+    work_sheet['Z' + str(count_row)] = sum[0]  # кол-во каналов
+    work_sheet['AA' + str(count_row)] = sum[1]  # кол-во таксофонов
+    work_sheet['AB' + str(count_row)] = sum[2]  # кол-во точек доступа
     columns_for_sum.clear()
 
-    # if count_row == 4809:
-    #     print('')
-
-    # if count_row == 4810:
-    #     print('')
-
     check_new_fias.append(i[1])
-    # count_row += 1
+
+############################################################################################################
 
 
 def data_writer(work_sheet, data):
@@ -144,6 +145,9 @@ def data_writer(work_sheet, data):
 
     is_ts = False
     is_uslugi_svyazi = False
+    is_gsm = False
+    is_umts = False
+    is_lte = False
 
     for i in data:
         spinner.next()
@@ -154,8 +158,76 @@ def data_writer(work_sheet, data):
 
         if i[1] not in check_new_fias:
             # запись новой строки (новый населенный пункт)
-            write_new_row(i, check_new_fias, work_sheet, max_speed,
-                   columns_for_sum, is_ts, is_uslugi_svyazi, count_row)
+            ts_str = 'нет'
+            uslugi_svyazi_str = 'нет'
+            gsm_str = 'нет'
+            umts_str = 'нет'
+            lte_str = 'нет'
+
+            if is_ts:
+                ts_str = 'да'
+            if is_uslugi_svyazi:
+                uslugi_svyazi_str = 'да'
+            if is_gsm:
+                gsm_str = 'да'
+            if is_umts:
+                umts_str = 'да'
+            if is_lte:
+                lte_str = 'да'
+
+            is_ts = False
+            is_uslugi_svyazi = False
+            is_gsm = False
+            is_umts = False
+            is_lte = False
+
+            work_sheet['W' + str(count_row)] = ts_str  # ТС
+            # Услуги связи
+            work_sheet['X' + str(count_row)] = uslugi_svyazi_str
+            work_sheet['T' + str(count_row)] = gsm_str  # GSM
+            work_sheet['U' + str(count_row)] = umts_str  # UMTS
+            work_sheet['V' + str(count_row)] = lte_str  # LTE
+
+            for row in work_sheet.iter_rows(min_row=count_row, max_row=count_row, min_col=8, max_col=19):
+                for cell in row:
+                    # print(cell.value)
+                    if cell.value is None:
+                        cell.value = 'нет'
+
+            # этот говнокод ничто иное, как суммарная информация по всем операторам
+            svod_mts = 'МТС - GSM - '+str(work_sheet['H' + str(count_row)].value)+'; МТС - UMTS - '+str(
+                work_sheet['I' + str(count_row)].value)+'; МТС - LTE - '+str(work_sheet['J' + str(count_row)].value)+'; '
+            svod_megafon = 'МегаФон - GSM - '+str(work_sheet['K' + str(count_row)].value)+'; МегаФон - UMTS - '+str(
+                work_sheet['L' + str(count_row)].value)+'; МегаФон - LTE - '+str(work_sheet['M' + str(count_row)].value)+'; '
+            svod_beeline = 'Билайн - GSM - '+str(work_sheet['N' + str(count_row)].value)+'; Билайн - UMTS - '+str(
+                work_sheet['O' + str(count_row)].value)+'; Билайн - LTE - '+str(work_sheet['P' + str(count_row)].value)+'; '
+            svod_tele2 = 'Теле2 - GSM - '+str(work_sheet['Q' + str(count_row)].value)+'; Теле2 - UMTS - '+str(
+                work_sheet['R' + str(count_row)].value)+'; Теле2 - LTE - '+str(work_sheet['S' + str(count_row)].value)+';'
+
+            work_sheet['AD' + str(count_row)] = svod_mts + \
+                svod_megafon + svod_beeline + svod_tele2
+
+            # замена ячеек с циферами на 'да'
+            if work_sheet['H' + str(count_row)].value != 'нет':
+                work_sheet['H' + str(count_row)] = 'да'
+            if work_sheet['K' + str(count_row)].value != 'нет':
+                work_sheet['K' + str(count_row)] = 'да'
+            if work_sheet['N' + str(count_row)].value != 'нет':
+                work_sheet['N' + str(count_row)] = 'да'
+            if work_sheet['Q' + str(count_row)].value != 'нет':
+                work_sheet['Q' + str(count_row)] = 'да'
+
+            # запись максимальной скорости
+            work_sheet['Y' + str(count_row)] = data_filter_max_speed(max_speed)
+            max_speed.clear()
+
+            sum = data_sum_columns(columns_for_sum)
+            work_sheet['Z' + str(count_row)] = sum[0]  # кол-во каналов
+            work_sheet['AA' + str(count_row)] = sum[1]  # кол-во таксофонов
+            work_sheet['AB' + str(count_row)] = sum[2]  # кол-во точек доступа
+            columns_for_sum.clear()
+
+            check_new_fias.append(i[1])
             count_row += 1
         # endregion
 
@@ -169,7 +241,7 @@ def data_writer(work_sheet, data):
         work_sheet['E' + str(count_row)] = i[4]  # город
         work_sheet['F' + str(count_row)] = i[5]  # район
         work_sheet['G' + str(count_row)] = i[6]  # населенный пункт
-        work_sheet['Z' + str(count_row)] = i[17]  # url для проверки'
+        work_sheet['AC' + str(count_row)] = i[17]  # url для проверки'
 
         # запись операторов мтс, мегафон, билайн, теле2
         if 'МТС' in i[7] or 'мтс' in i[7]:
@@ -193,16 +265,25 @@ def data_writer(work_sheet, data):
             is_ts = True
         if i[9] == 'да':
             is_uslugi_svyazi = True
+        # gsm
+        if i[11] == '900' or i[11] == '1800' or i[11] == '900/1800':
+            is_gsm = True
+        if i[12] == 'да':
+            is_umts = True
+        if i[13] == 'да':
+            is_lte = True
 
         count += 1
 
         # запись последней строки (иначе не пишет данные по операторам и т.д.)
         if count == len(data):
-            write_new_row(i, check_new_fias, work_sheet, max_speed,
-                   columns_for_sum, is_ts, is_uslugi_svyazi, count_row)
+            write_last_row(i, check_new_fias, work_sheet, max_speed,
+                           columns_for_sum, is_ts, is_uslugi_svyazi, is_gsm, is_umts, is_lte, count_row)
 
     spinner.finish()
     return
+
+############################################################################################################
 
 
 def write_xlsx_file(data):
@@ -231,16 +312,19 @@ def write_xlsx_file(data):
     work_sheet['Q1'] = 'Теле2 GSM'
     work_sheet['R1'] = 'Теле2 UMTS'
     work_sheet['S1'] = 'Теле2 LTE'
-    work_sheet['T1'] = 'Услуги местной ТС'
-    work_sheet['U1'] = 'Телематические услуги связи'
+    work_sheet['T1'] = 'Наличие GSM'
+    work_sheet['U1'] = 'Наличие UMTS'
+    work_sheet['V1'] = 'Наличие LTE'
+    work_sheet['W1'] = 'Услуги местной ТС'
+    work_sheet['X1'] = 'Телематические услуги связи'
     # телематические услуги связи (скорость) и единицы измероение в одну ячейку
-    work_sheet['V1'] = 'Телематические услуги связи (максимальная скорость передачи данных)'
+    work_sheet['Y1'] = 'Телематические услуги связи (максимальная скорость передачи данных)'
     # сумма
-    work_sheet['W1'] = 'Эфирное телевидение (количество цифровых каналов)'
-    work_sheet['X1'] = 'Количество таксофонов'  # сумма
-    work_sheet['Y1'] = 'Количество точек доступа'  # сумма
-    work_sheet['Z1'] = 'URL'  # url для проверки
-    work_sheet['AA1'] = 'Доп. информация'
+    work_sheet['Z1'] = 'Эфирное телевидение (количество цифровых каналов)'
+    work_sheet['AA1'] = 'Количество таксофонов'  # сумма
+    work_sheet['AB1'] = 'Количество точек доступа'  # сумма
+    work_sheet['AC1'] = 'URL'  # url для проверки
+    work_sheet['AD1'] = 'Доп. информация'
 
     # дополнительная информация в виде:
     # 4 основных оператора:
@@ -260,12 +344,13 @@ def write_xlsx_file(data):
             "Файл открыт в другой программе. Закройте файл и повторите попытку")
         return
 
+############################################################################################################
+
 
 def read_csv_file(file_name):
     data = []
     count = 0
 
-    # file_data_before_sort = open('data_before_sort.txt', 'w')
     # file_data_sort = open('data_sort.txt', 'w')
 
     try:
@@ -285,12 +370,22 @@ def read_csv_file(file_name):
             # сортируем по населенному пункту и району (фиас для контроля, но и без него заэбись)
             data = sorted(data, key=itemgetter(6, 5))
 
+            # n = []
+            # for i in data:
+            #     # if i[1] not in n:
+            #     #     n.append(i[1])
+            #     file_data_sort.write(str(i[1]) + ' - ' + str(i[3]) + ' - ' + str(i[4]) + ' - ' + str(
+            #         i[5]) + ' - ' + str(i[6]) + ' -( ' + str(i[8]) + ' - ' + str(i[9]) + ' )-{ ' + ' - ' + str(i[11]) + ' - ' + str(i[12]) + ' - ' + str(i[13]) + ' - ' + str(i[7]) + '}\n')
+            # file_data_sort.close()
+
     except IOError:
         print(
             "Файл открыт в другой программе. Закройте файл и повторите попытку")
         return
 
     return data
+
+############################################################################################################
 
 
 def main():
